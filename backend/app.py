362a515ffac09d74fe9525a4b4a888d20e200623
Safi-Ahmed-Shariff <safiahmedshariff@gmail.com>
2024-11-20 23:43:20 +0530

    made change in database url

--- backend/app.py
+++ backend/app.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 import os
 from sqlalchemy import create_engine
 
-DATABASE_URL = f"postgresql://{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}@{os.getenv('POSTGRES_HOST')}:{os.getenv('POSTGRES_PORT')}/{os.getenv('POSTGRES_DB')}"
+DATABASE_URL = f"postgresql://{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}@localhost:5432//{os.getenv('POSTGRES_DB')}"
 engine = create_engine(DATABASE_URL)
 
 load_dotenv()
